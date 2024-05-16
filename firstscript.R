283033e52e2aae16945f55159d97e59000837fe8
Melanie Beebe <mbeebe@ncsu.edu>
2024-05-16 16:01:25 -0400

    add firstscript

---
+++ firstscript.R
@@ -0,0 +1,31 @@
+iris
+head(iris)
+tail(iris)
+help("seq")
+seq(0, 1, length.out = 11)
+seq(1, 9, by = 3)   
+avg <- (5 + 7 + 6) / 3
+avg
+cars
+mean(cars$speed)
+data()
+women
+mean(women$height)
+mean(women$weight)
+iris <- iris
+iris
+class(cars)
+class(exp)
+plot(cars)
+plot(exp)
+hist(cars$speed)
+fit <- lm(dist ~ speed, data = cars)
+fit
+str(iris)
+hist(iris$Sepal.Length)
+?hist
+SW <- iris$Sepal.Width
+SW
+class(SW)
+is.vector(SW)
+cat("Hi. My name is Melanie. I am a student at NC State")
